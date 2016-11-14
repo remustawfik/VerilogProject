b61f868a8234849efa5b210a0816450cee183911
Reem Tawfik <reemayman@gmail.com>
2016-11-13 23:56:47 -0500

    Add files via upload

---
+++ FuckMeUpFirst.v
@@ -0,0 +1,21 @@
+module FuckMeUpFirst(LEDR,GPIO_1,GPIO_0,CLOCK_50);
+input [35:0]GPIO_1;
+input CLOCK_50;
+input [35:0]GPIO_0;
+output [9:0] LEDR;
+
+wire receiver;
+reg led;
+assign LEDR[0] = led;
+assign receiver = GPIO_1[1];
+
+always@(*)
+begin
+led<= 1'b0;
+if(receiver == 1'b1)
+led <= 1'b1;
+else
+led<= 1'b0;
+end
+
+endmodule 
