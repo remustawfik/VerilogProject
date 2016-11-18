8bf6a04807325dc24fe2fc63463546eab62904dc
Reem Tawfik <reemayman@gmail.com>
2016-11-17 22:38:25 -0500

    Add files via upload

--- OnePunchMan.v
+++ OnePunchMan.v
@@ -1,9 +1,19 @@
 
 
-module OnePunchMan(CLOCK_50, KEY,VGA_CLK,VGA_HS,VGA_VS,VGA_BLANK_N,VGA_SYNC_N,VGA_R,VGA_G,VGA_B);
+module OnePunchMan(CLOCK_50, KEY,LEDR,VGA_CLK,VGA_HS,VGA_VS,VGA_BLANK_N,VGA_SYNC_N,VGA_R,VGA_G,VGA_B);
 
 input CLOCK_50;
 input [3:0] KEY;
+
+output			VGA_CLK;   				//	VGA Clock
+output			VGA_HS;					//	VGA H_SYNC
+output			VGA_VS;					//	VGA V_SYNC
+output			VGA_BLANK_N;		   //	VGA BLANK
+output			VGA_SYNC_N;				//	VGA SYNC
+output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+output [9:0] LEDR;
 
 wire [8:0] xVGA;
 wire [7:0] yVGA;
@@ -15,33 +25,23 @@ wire [2:0] addressColour;
 
 
 //Flags
-wire plotIt;
-wire SoDone;
-wire right;
-wire reset;
-wire delete;
-wire dataDel;
-wire startDraw;
+wire plot,reset,start;
+
+//datapath output flags
+wire Ddraw,Dwait,Ddel;
+
+//control output flags
+wire Swait,Sdel,SresetDel;
+
 
 assign reset = KEY[0];
-assign plotIt = ~KEY[1];
-assign right = ~KEY[2];
-assign center = ~KEY[3];
-
-
-output			VGA_CLK;   				//	VGA Clock
-output			VGA_HS;					//	VGA H_SYNC
-output			VGA_VS;					//	VGA V_SYNC
-output			VGA_BLANK_N;		   //	VGA BLANK
-output			VGA_SYNC_N;				//	VGA SYNC
-output	[9:0]	VGA_R;   				//	VGA Red[9:0]
-output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
-output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
-
+assign start = ~KEY[1];
+assign LEDR[0] = Ddraw; 
 
 Boxer cat(address,CLOCK_50,3'b000,1'b0,addressColour);
 
-assign colourVGA = dataDel ? 3'b000 : addressColour;
+assign colourVGA = Sdel ? 3'b000 : addressColour;
+assign plot = 1'b1;
 
 vga_adapter VGA(
 			.resetn(reset),
@@ -49,7 +49,7 @@ vga_adapter VGA(
 			.colour(colourVGA),
 			.x(xVGA),
 			.y(yVGA),
-			.plot(plotIt),
+			.plot(plot),
 			.VGA_R(VGA_R),
 			.VGA_G(VGA_G),
 			.VGA_B(VGA_B),
@@ -64,30 +64,29 @@ vga_adapter VGA(
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 		defparam VGA.BACKGROUND_IMAGE = "bannertoo.mif";
 		
-control pupperz(CLOCK_50,reset,SoDone,plotIt,center,right,xCtrl,yCtrl,dataDel);
-datapath PUPPER(CLOCK_50,reset,plotIt,SoDone,xCtrl,yCtrl,xVGA,yVGA,address,dataDel);	
+control pupperz(CLOCK_50,reset,Ddraw,Ddel,Dwait,start,xCtrl,yCtrl,Sdel,Swait,SresetDel);
+datapath PUPPER(CLOCK_50,reset,start,Ddraw,Ddel,Dwait,xCtrl,yCtrl,xVGA,yVGA,address,Sdel,Swait,SresetDel);
 			
 endmodule 
 
 //-------------------------------------------------------------------------------------
-module control(clk,reset,DoneCtrl,PlotCtrl,centerflag,deleteFlag,xout,yout,delpls);
+module control(clk,reset,DoneDraw,DoneDel,DoneWait,PlotCtrl,xout,yout,startDel,startWait,startresetDel);
 
-input clk,reset,DoneCtrl,PlotCtrl,centerflag,deleteFlag;
-//input [2:0] Cin;
+input clk,reset,DoneDraw,PlotCtrl,DoneDel,DoneWait;
 output reg [8:0] xout;
 output reg [7:0] yout;
-//output reg [2:0] Cout;
-output reg delpls;
+output reg startDel,startWait,startresetDel;
 
 
 
 reg [5:0] current_state, next_state; 
     
-    localparam  S_Reset         = 3'd0, 
-					 S_StartAnimation= 3'd1, 
+    localparam  S_Reset          = 3'd0, 
+					 S_StartAnimation = 3'd1, 
 					 S_Center         = 3'd2,
-					 S_Delete          = 3'd3,
-					 S_Done          = 3'd4;
+					 S_Wait           = 3'd3,
+					 S_Delete         = 3'd4,
+					 S_Done           = 3'd5;
 					 
 					 
 	always@(*)
@@ -98,16 +97,15 @@ reg [5:0] current_state, next_state;
 					  
 				S_Reset: next_state =  PlotCtrl ? S_StartAnimation: S_Reset;
 				
-				S_StartAnimation: next_state = deleteFlag ? S_Center : S_Delete;
+				S_StartAnimation: next_state = S_Center;
 				
-				S_Center : next_state = DoneCtrl ? S_Done : S_Center;
+				S_Center : next_state = DoneDraw ? S_Wait : S_Center;
 				
-				S_Delete : next_state = DoneCtrl ? S_Done : S_Delete;
+				S_Wait: next_state = DoneWait ? S_Delete : S_Wait;
+				
+				S_Delete : next_state = DoneDel ? S_Done : S_Delete;
 				
 				S_Done: next_state = S_Reset;
-				
-//				S_Delete: next_state = S_Reset;
-				
 				
 				default:
 				next_state= S_Reset;
@@ -127,29 +125,30 @@ reg [5:0] current_state, next_state;
 	
 	xout = 9'd120;
 	yout = 8'd70;
-	delpls = 1'b0;
-	//Cout = Cin;
+	startDel = 1'b0;
+	startWait = 1'b0;
+	startresetDel = 1'b0;
+	
+	end
+	
+	S_Wait: begin
+	
+	startWait = 1'b1;
+	startDel = 1'b0;
+	startresetDel = 1'b1;
 	
 	end
 	
 	S_Delete: begin
 	
-   xout = 9'd0;
-	yout = 8'd70;
-	delpls = 1'b1;
-	//Cout = 3'b000;
-	
+	startDel = 1'b1;
+	startWait = 1'b0;
+	startresetDel = 1'b0;
+
 	end
 	
-//	S_Delete: begin
-//	
-//	
-//  xout = 9'd80;
-//	yout = 8'd70;
-//	cout = 3'b000;
-//	draw = 1'b1;
-//	
-//   end
+	
+	
 endcase
 end
 
@@ -172,294 +171,103 @@ always@(posedge clk)
  //----------------------------------------------------------------------------------------------
  
  
- module datapath(clk,resetData, start,DoneData,Xin,Yin,Xout,Yout,counter,delete);
+ module datapath(clk,resetData,Plot,DoneDrawing,DoneDeleting,DoneWaiting,Xin,Yin,Xout,Yout,Drawcount,startDelete,startWait,startresDel);
  
- input clk,resetData,start,delete;
+ input clk,resetData,Plot,startDelete,startWait,startresDel;
  
  input [8:0] Xin;
  input [7:0] Yin;
+ 
+ //output flags
+ output reg DoneDrawing,DoneDeleting;
+ output DoneWaiting;
+ 
+ //accessing memory block and VGA
+ output reg [13:0] Drawcount;
  output reg [8:0] Xout;
  output reg [7:0] Yout;
- output reg DoneData;
- output reg [13:0]counter;
+ 
+ 
  wire [8:0] Xlimit;
  wire [7:0] Ylimit;
+ reg [27:0] timer;
  
+ wire clear1;
  
- 
- assign Xlimit = delete ? (Xin + 9'd320) : (Xin + 9'd80);
+ assign Xlimit = Xin + 9'd80;
  assign Ylimit = Yin + 8'd120;
 
- //--------------------------------------------------------
-//	always@(posedge CLOCK_50)
-//	begin
-//		if((clear1 == 1'b1)|(reset == 1'b1))
-//			count1 <= 28'd0;
-//		else
-//			count1 <= count1 + 1'b1;
-//	end
-//
-//	assign clear1 = Enable;
-//	assign Enable = (count1 == maxCount) ? 1'b1 : 1'b0;
-//
-//	always@(posedge CLOCK_50)
-//	begin
-//		if(reset == 1'b1)
-//			count2 <= 4'b0000;
-//		else if(Enable == 1'b1)
-//			count2 <= count2 + 1'b1;
-//		else
-//			count2 <= count2;
-//	end
+ //counts to 4 seconds
+ 
+	always@(posedge clk)
+	begin
+	if(clear1 | !resetData)
+			timer <= 28'd0;	
+	if(startWait)
+			timer <= timer + 1'b1;
+	end
+
+	assign clear1 = DoneWaiting;
+	assign DoneWaiting = (timer == 28'd200000000) ? 1'b1 : 1'b0;
+
 //---------------------------------------------------------
  always@(posedge clk)
    begin
-	if(!resetData)
-		begin
-			Xout <= Xin;
-			Yout <= Yin;
-			DoneData <= 1'b0;
-			counter <= 14'd0;
-		end
+				if(!resetData)
+					begin
+						Xout <= Xin;
+						Yout <= Yin;
+						DoneDrawing <= 1'b0;
+						Drawcount <= 14'd0;
+					end
+				
+				if(startresDel)
+				begin
+						Xout <= 9'd0;
+						Yout <= 9'd70;
+						Drawcount <= 14'b0;
+						DoneDeleting <= 1'b0;
+				end
 	
-	if(start)
-		begin
-	  if(!delete)
-		  begin
-		if(Xout == Xlimit && Yout < Ylimit)
-			begin
-			Xout <= Xin;
-			Yout <= Yout + 1'b1;
-			DoneData <= 1'b0;
-			end
-		if(Yout == Ylimit)
-			begin
-			DoneData <= 1'b1;
-			end
-		if(Xout < Xlimit && Yout < Ylimit)
-			begin
-				counter <= counter + 1'b1;
-				Xout <= Xout + 1'b1;
-				DoneData <= 1'b0;
-			end
-		end
-	 else
-		begin
-		if(Xout == 9'd320 && Yout < 9'd190)
-			begin
-			Xout <= 9'd0;
-			Yout <= Yout + 1'b1;
-			DoneData <= 1'b0;
-			counter <= 1'b0;
-			end
-		if(Yout == 9'd190)
-			begin
-			counter <= 1'b0;
-			DoneData <= 1'b1;
-			end
-		if(Xout < 9'd320 && Yout < 9'd190)
-			begin
-				counter <= 1'b0;
-				Xout <= Xout + 1'b1;
-				DoneData <= 1'b0;
-			end
-		
-		end
-	end
+				if(Plot)
+					begin
+						if(Xout == Xlimit && Yout < Ylimit)
+							begin
+							Xout <= Xin;
+							Yout <= Yout + 1'b1;
+							DoneDrawing <= 1'b0;
+							end
+						if(Yout == Ylimit)
+							begin
+							DoneDrawing <= 1'b1;
+							end
+						if(Xout < Xlimit && Yout < Ylimit)
+							begin
+								Drawcount <= Drawcount + 1'b1;
+								Xout <= Xout + 1'b1;
+								DoneDrawing <= 1'b0;
+							end
+					end
+				 if(startDelete & !startresDel)
+						begin
+						if(Xout == 9'd320 && Yout < 9'd190)
+							begin
+							Xout <= 9'd0;
+							Yout <= Yout + 1'b1;
+							DoneDeleting <= 1'b0;
+							
+							end
+						if(Yout == 9'd190)
+							begin
+							DoneDeleting <= 1'b1;
+							end
+						if(Xout < 9'd320 && Yout < 9'd190)
+							begin
+								Xout <= Xout + 1'b1;
+								DoneDeleting <= 1'b0;
+							end
+	               end
+
 	end
 	
-endmodule
- 
-// 
-//module OnePunchMan(CLOCK_50, KEY,VGA_CLK,VGA_HS,VGA_VS,VGA_BLANK_N,VGA_SYNC_N,VGA_R,VGA_G,VGA_B);
-//
-//input CLOCK_50;
-//input [3:0] KEY;
-//
-//wire [8:0] xVGA;
-//wire [7:0] yVGA;
-//wire [2:0] colourVGA;
-//wire [13:0] address;
-//wire plotIt;
-//wire SoDone;
-//wire reset;
-//
-//assign reset = KEY[0];
-//assign plotIt = ~KEY[1];
-//
-//
-//output			VGA_CLK;   				//	VGA Clock
-//output			VGA_HS;					//	VGA H_SYNC
-//output			VGA_VS;					//	VGA V_SYNC
-//output			VGA_BLANK_N;		   //	VGA BLANK
-//output			VGA_SYNC_N;				//	VGA SYNC
-//output	[9:0]	VGA_R;   				//	VGA Red[9:0]
-//output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
-//output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
-//
-//
-//Boxer cat(address,CLOCK_50,3'b000,1'b0,colourVGA);
-//
-//vga_adapter VGA(
-//			.resetn(reset),
-//			.clock(CLOCK_50),
-//			.colour(colourVGA),
-//			.x(xVGA),
-//			.y(yVGA),
-//			.plot(plotIt),
-//			.VGA_R(VGA_R),
-//			.VGA_G(VGA_G),
-//			.VGA_B(VGA_B),
-//			.VGA_HS(VGA_HS),
-//			.VGA_VS(VGA_VS),
-//			.VGA_BLANK(VGA_BLANK_N),
-//			.VGA_SYNC(VGA_SYNC_N),
-//			.VGA_CLK(VGA_CLK));
-//			
-//		defparam VGA.RESOLUTION = "320x240";
-//		defparam VGA.MONOCHROME = "FALSE";
-//		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
-//		defparam VGA.BACKGROUND_IMAGE = "bannertoo.mif";
-//		
-//		
-//control pupperz(CLOCK_50,reset,SoDone,plotIt);
-//datapath PUPPER(CLOCK_50,reset,plotIt,SoDone,xVGA,yVGA,address);	
-//			
-//endmodule 
-//
-////-------------------------------------------------------------------------------------
-//module control(clk,reset,DoneCtrl,PlotCtrl);
-//
-//input clk,reset,DoneCtrl,PlotCtrl;
-//
-//reg [5:0] current_state, next_state; 
-//    
-//    localparam  S_Reset         = 3'd0, 
-//					 S_StartAnimation= 3'd1,                
-//					 S_Done          = 3'd2;
-//					 
-//					 
-//	always@(*)
-//   
-//		 begin: state_table 
-//				 
-//			  case (current_state)
-//					  
-//				S_Reset: next_state =  PlotCtrl ? S_StartAnimation: S_Reset;
-//				
-//				S_StartAnimation: next_state = DoneCtrl ? S_Done : S_StartAnimation;
-//				
-//				S_Done: next_state = S_Reset;
-//				
-//				
-//				default:
-//					next_state= S_Reset;
-//					
-//		 endcase
-//   end
-//// 
-//// always @(*)
-////  
-////  begin: enable_signals
-////       
-//// 
-////  load_Done = 1'b0; 
-////  
-////  
-//// case (current_state)
-////           
-////
-////	S_Reset: begin
-////	
-////	
-////		
-////   end
-////	
-////	S_StartAnimation: begin
-////	
-////	end
-////	
-////	S_Done: begin
-////	
-////	
-////   end
-////endcase
-////end
-//
-//// current_state registers
-//  
-//always@(posedge clk)
-//  
-//   begin: state_FFs
-//     
-//      if(!reset)       
-//         current_state <= S_Reset; 
-//      else            
-//		   current_state <= next_state;
-// 
-//   end // state_FFS
-//
-// 
-// endmodule
-// 
-// //----------------------------------------------------------------------------------------------
-// 
-// 
-// module datapath(clk,resetData, PlotData, DoneData,Xout,Yout,counter);
-// 
-// input clk,resetData,PlotData;
-// output reg [8:0] Xout;
-// output reg [7:0] Yout;
-// //output reg [2:0] Cout;
-// output reg DoneData;
-//
-//// reg [8:0] Xorigin;
-//// reg [7:0] Yorigin;
-//// reg [2:0] regColor;
-// output reg [13:0]counter;
-// wire [8:0] Xlimit;
-// wire [7:0] Ylimit;
-// 
-// assign Xlimit = 9'd160 +  9'd80;
-// assign Ylimit = 8'd70 + 8'd120;
-// 
-// always@(posedge clk)
-//   begin
-//	if(!resetData)
-//		begin
-//			Xout <= 9'd160;
-//			Yout <= 8'd70;
-//			DoneData <= 1'b0;
-//			counter <= 15'b0;
-//			//Cout <= 3'b001 ;
-//		end
-//	
-//	if(PlotData)
-//	begin
-//	if(Xout == Xlimit && Yout < Ylimit)
-//		begin
-//		Xout <= 9'd160;
-//		Yout <= Yout + 1'b1;
-//		DoneData <= 1'b0;
-//		end
-//	if(Yout == Ylimit)
-//		begin
-//		DoneData <= 1'b1;
-//		end
-//	if(Xout < Xlimit && Yout < Ylimit)
-//		begin
-//			counter <= counter + 1'b1;
-//			Xout <= Xout + 1'b1;
-//		end
-//	end
-//	end
-//	
-//endmodule
-// 
-// //120: 1111000
-// //80: 1010000
-// //60: 111100
-// //160:10100000
-// //70:01000110
-// 					 
-//					
+endmodule 
